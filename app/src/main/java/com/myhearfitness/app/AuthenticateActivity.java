33464a49e500a26b26eba2f71edad706ca731235
evasga98 <evasga98@gmail.com>
2021-03-09 21:07:59 +0100

    fingerprint/pin authentication

---
+++ app/src/main/java/com/myhearfitness/app/AuthenticateActivity.java
@@ -0,0 +1,103 @@
+package com.myhearfitness.app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import static androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG;
+import static androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL;
+
+
+public class AuthenticateActivity extends AppCompatActivity {
+
+
+    private BiometricPrompt biometricPrompt=null;
+    private Executor executor = Executors.newSingleThreadExecutor();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticate);
+
+        if(biometricPrompt==null){
+            biometricPrompt=new BiometricPrompt(this,executor,callback);
+        }
+        checkAndAuthenticate();
+
+    }
+
+    private void checkAndAuthenticate(){
+        BiometricManager biometricManager=BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate())
+        {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                BiometricPrompt.PromptInfo promptInfo = buildBiometricPrompt();
+                biometricPrompt.authenticate(promptInfo);
+
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                snack("Biometric Authentication currently unavailable");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                snack("Your device doesn't support Biometric Authentication");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                snack("Your device doesn't have any fingerprint enrolled");
+                break;
+        }
+    }
+    private BiometricPrompt.PromptInfo buildBiometricPrompt()
+    {
+        return new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Login")
+                .setSubtitle("FingerPrint Authentication")
+                .setDescription("Please place your finger on the sensor to unlock")
+                .setAllowedAuthenticators(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)
+                .build();
+
+    }
+
+    private BiometricPrompt.AuthenticationCallback callback=new
+            BiometricPrompt.AuthenticationCallback() {
+                @Override
+                public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                    if(biometricPrompt!=null)
+                        biometricPrompt.cancelAuthentication();
+                    snack((String) errString);
+                }
+
+                @Override
+                public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                    snack("Authenticated");
+                    changeActivity();
+
+                }
+
+                @Override
+                public void onAuthenticationFailed() {
+                    snack("The FingerPrint was not recognized.Please Try Again!");
+                }
+            };
+
+    private void snack(String text)
+    {
+        View view=findViewById(R.id.authenticate_view);
+        Snackbar snackbar=Snackbar.make(view,text, Snackbar.LENGTH_LONG);
+        snackbar.show();
+    }
+
+    private void changeActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
+    }
+}
